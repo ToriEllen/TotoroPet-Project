72ffd0c057b79a0c9fdcc84075e2f4dcdc53f130
Vitória Ellen Freitas <vitoriaelenfreitas1@gmail.com>
2025-10-12 14:16:55 -0300

    style: favicon, e nav bar nas páginas

--- src/JavaScript/script.js
+++ src/JavaScript/script.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
+ 
   function animateOnScroll() {
     document.querySelectorAll('[data-animate]').forEach(function(el) {
-      const rect = el.getBoundingClientRect();
+      const rect = el.getBoundingClientRect();//retorna um objeto com as posições e tamanhos do elemento na tela, em relação à janela de visualização do usuario.
       if (rect.top < window.innerHeight - 50) {
-        el.classList.add('visible');
+        el.classList.add('visible');//aqui interagimos diretamente com o css acionando a classe visible
       }
     });
   }
 
   window.addEventListener('scroll', animateOnScroll);
-  animateOnScroll(); // Para animar os elementos já visíveis ao carregar
+  animateOnScroll(); // Para animar os elementos  que já estão visíveis quando a pagina carrega
 });
 
+//function slider, galeria de fotos
+
 let cont = 1
-const totalRadio = 3
+const totalRadio = 4
 
 setInterval (() =>{
     document.getElementById('radio' + cont).checked = true
