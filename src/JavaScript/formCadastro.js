e90ebbf4d13bb2da26c7c8e2ee7535d6c4381f66
Vitória Ellen Freitas <vitoriaelenfreitas1@gmail.com>
2025-10-22 22:30:02 -0300

    feat:Validações e animações na pagina de cadastro

--- src/JavaScript/formCadastro.js
+++ src/JavaScript/formCadastro.js
@@ -1,3 +1,4 @@
+//efeito de virar o card de login/cadastro com css
 const card = document.getElementById('card');
 const btnEntrar = document.getElementById('btnEntrar');
 const btnCadastrar = document.getElementById('btnCadastrar');
@@ -10,11 +11,109 @@ btnCadastrar.addEventListener('click', () => {
   card.classList.remove('virar');
 });
 
-//ajeitar evento alert
-const buttons = document.getElementsByClassName('submit-button');
- buttons.forEach(button => {
+//validação de formulários de cadastro e login
+const buttons = document.querySelectorAll('.submit-button');
+
+//adiciona evento de click para todos os botões com a classe submit-button
+    buttons.forEach(button => {
     button.addEventListener('click', function(event) {
     event.preventDefault();
-    alert('Cadastro realizado com sucesso!');
+
+    const form = button.closest('form');//encontra o form mais próximo do botão clicado.
+    const inputs = form.querySelectorAll('input');//variavel que seleciona todos os inputs dentro do form  mais proximo do botão clicado.
+    let camposVazios = false;
+
+    //função para limpar mensagem de erro e classe css
+        function limparErro (input, errorMsg){
+      input.classList.remove('error');
+      errorMsg.textContent = '';
+      errorMsg.style.display = 'none';
+       input.style.border = '1px solid #ccc';
+    }
+
+    //validação de campos vazios e adiciona mensagem de erro e class css, se nao estiver vazio limpa a mensagem de erro
+    inputs.forEach(input => {
+       const errorMsg = input.nextElementSibling;
+      if (input.value.trim() === ""){
+        camposVazios = true;
+        input.classList.add('error');
+        input.style.border = '1px solid red';
+        errorMsg.textContent = 'Campo obrigatório';
+        errorMsg.style.display = 'block';
+      }else{
+        limparErro(input, errorMsg);
+      }
+    
+
+     input.addEventListener('input', () => 
+        limparErro(input, errorMsg));
     });
+
+//mensagens de sucesso no window alert para as duas formas
+    if (!camposVazios && button.id === 'btnCadastro') {
+      alert('Cadastro realizado com sucesso!');
+    }else if (!camposVazios && button.id === 'btnLogin') {
+      alert('Login realizado com sucesso!');
+    }
+  });
 });
+
+const senhaInput = document.querySelectorAll('#senha');
+const toggleSenha = document.querySelectorAll('#toggleSenha');
+
+//forEach para pegar todos os elementos com id toggleSenha e adicionar evento de click
+/*toggleSenha.forEach(toggle => {
+  toggle.addEventListener("click", () => {
+    const senhaInput = toggle.previousElementSibling;
+    const tipo = senhaInput.getAttribute("type") === "password" ? "text" : "password";
+    senhaInput.setAttribute("type", tipo);
+    toggle.classList.toggle("fa-eye-slash");
+  });
+});*/
+
+
+//ajeitar o time das trocas de imagens
+
+const animacoes = [
+  '../images/Animation1.png',
+  '../images/Animation2.png', 
+  '../images/Animation3.png',
+];
+
+const imagens = document.querySelectorAll('.animation-img');
+
+function trocarImagem(index) {
+  imagens.forEach(img => {
+    img.src = animacoes[index];
+    img.style.transition = '1s ease-in-out';
+  });
+}
+ 
+
+trocarImagem(0);
+
+const senhaInputs = document.querySelectorAll('#senha');
+const toggleSenhas = document.querySelectorAll('#toggleSenha');
+
+senhaInputs.forEach(input => {
+  input.addEventListener("click", () => {
+    trocarImagem(1); 
+  });
+
+   input.addEventListener('mouseout', () => {
+    trocarImagem(0);
+  });
+});
+
+toggleSenhas.forEach(toggle => {
+  toggle.addEventListener("click", () => {
+    const senhaInput = toggle.previousElementSibling;
+    const tipo = senhaInput.getAttribute("type") === "password" ? "text" : "password";
+    senhaInput.setAttribute('type', tipo);
+    toggle.classList.toggle("fa-eye-slash");
+
+    trocarImagem(2);
+  });
+});
+
+
