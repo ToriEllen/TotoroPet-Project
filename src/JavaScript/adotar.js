ca1b85e1985027dfd9e061cebb5b7b010e78bfc5
anavitoria-pixel <anavitoriadpsoares@gmail.com>
2025-11-15 15:31:28 -0300

    style: form adoção

--- src/JavaScript/adotar.js
+++ src/JavaScript/adotar.js
@@ -7,10 +7,26 @@ imagens.forEach((img, i) => {
     }, i * 400);
 })*/
 
-function populateList(){
+/*function populateList(){
     const data = Array.from({ length: 9})
     .map((_, i) => `<div class="item">Item ${(i + 1)}</div`)
 
     const list = document.querySelector('#pagination .list')
     list.innerHTML = data.join("")
 }
+
+document.getElementById("btnAdotar").addEventListener("click", () => {
+    document.getElementById("modalForm").style.display = "flex";
+});
+
+// Fechar o form
+document.getElementById("closeForm").addEventListener("click", () => {
+    document.getElementById("modalForm").style.display = "none";
+});
+
+// Fechar clicando fora do form
+document.getElementById("modalForm").addEventListener("click", (e) => {
+    if (e.target.id === "modalForm") {
+        document.getElementById("modalForm").style.display = "none";
+    }
+});*/
